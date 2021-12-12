8d31e20e5b97d5fa834d63db37e2456cb6018e83
Indraneel Dutta <meneel2000@gmail.com>
2021-12-12 11:31:38 +0530

    First coomit

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject Bird;
+    public GameObject Pattern;
+    public float health = 4f;
+    public static int enemiesAlive = 0;
+
+    private void Start()
+    {
+        enemiesAlive++;
+    }
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            DIE();
+        }
+    }
+
+    void DIE()
+    {
+        Instantiate(Pattern, transform.position, Quaternion.identity);
+        enemiesAlive--;
+        if(enemiesAlive<=0)
+        {
+            Debug.Log("LEVEL WON!");
+        }
+        Destroy(Bird);
+
+
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Projectile : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private SpringJoint2D sp;
+    public GameObject NextBall;
+    private bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpringJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.15f);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+        if(NextBall != null)
+        {
+            NextBall.SetActive(true);
+        }
+        else
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
